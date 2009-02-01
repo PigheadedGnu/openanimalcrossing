7b4b5e3a3d2188daf6c5f4a85f06b93e388ca38e
imaginationac <imaginationac@89a9656c-5848-0410-a18d-3f75b71fd5a3>
2009-02-01 05:01:15 +0000

    Placed comments at the head of some files.

--- trunk/src/Items/Furniture.java
+++ trunk/src/Items/Furniture.java
@@ -1,13 +1,26 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package Items;
 
 /**
+ * Project: OpenAnimalCrossing
+ * File: NonPlayerCharacter.java
+ * Represents any placable piece of furniture (chairs, beds, tables, dressers,
+ * etc.)
+ * Last modified: 11:56 PM EST, Saturday, January 31, 2009.
  *
- * @author Dorian
+* Copyright (C) 2008 Dorian Patterson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 public class Furniture extends Item implements isMovable, canRotate{
      public Furniture(String name, int price){
--- trunk/src/Items/canRotate.java
+++ trunk/src/Items/canRotate.java
@@ -1,13 +1,24 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package Items;
-
 /**
+ * Project: OpenAnimalCrossing
+ * File: NonPlayerCharacter.java
+ * Inteface that indicates an item can rotate.
+ * Last modified: 11:57 PM EST, Saturday, January 31, 2009.
  *
- * @author Dorian Patterson
+ * Copyright (C) 2009 Dorian Patterson.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 interface canRotate {
 
